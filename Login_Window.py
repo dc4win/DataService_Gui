f3fed1fd1635b699c5b4a7b71f9c0f8587a8e76a
dc4win <dc1014549257dc@gmail.com>
2022-01-10 22:05:18 +0800

    add

--- Login_Window.py
+++ Login_Window.py
@@ -7,8 +7,19 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QApplication, QLabel, QDesktop
 
 class LoginForm(QWidget):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.initUI()
 
     def initUI(self):
         """初始化UI"""
+        self.setObjectName("loginwindow")
+        self.setStyleSheet('#loginwindow{background-color:black}')
+        self.setFixedSize(650,400)
+        self.setWindowTitle("登录界面")
+        self.setWindowIcon(QIcon('blue-sky-with-windy-clouds.jpg'))
+        self.text = "镇江气象数据服务平台"
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    ex = LoginForm()
+    sys.exit(app.exec_())
